4da7ad46e3f33a97883827735d780f48995f069a
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2024-09-10 17:21:03 -0700

    clear timeout when deleting reminder

--- src/client/reminders-scheduler.ts
+++ src/client/reminders-scheduler.ts
@@ -1,18 +1,22 @@
 import { Client, Message, TextChannel } from "discord.js";
 
 import db from "../db";
+import replies from "./replies";
 import { Reminder } from "../db/models";
 import { Cache, cache, nicknameMention, Time } from "../utils";
-import replies from "./replies";
 
 const REMINDERS_FETCH_LIMIT = 30;
 let initiated: boolean = false;
 
-// TODO: cancel timeout and remove from cache if reminder is deleted
 const remindersSchedulerCache: Cache = cache.new("timeoutCache");
 
 const deleteReminder = (id: string): void => {
-  remindersSchedulerCache.delete(id);
+  const result = remindersSchedulerCache.get(id);
+
+  if (result) {
+    clearTimeout(result.timeout);
+    remindersSchedulerCache.delete(id);
+  }
 };
 
 const handler = async (client: Client, reminder: Reminder): Promise<void> => {
@@ -29,7 +33,7 @@ const handler = async (client: Client, reminder: Reminder): Promise<void> => {
 };
 
 const notify = async (client: Client, reminderId: string): Promise<void> => {
-  const reminder: Reminder = remindersSchedulerCache.get(reminderId);
+  const { reminder } = remindersSchedulerCache.get(reminderId);
   const channel = client.channels.cache.find(c => c.id === reminder.channelId) as TextChannel;
   await channel.send(`Reminder for ${nicknameMention(reminder.userId)}:\n${reminder.message}`);
 };
@@ -42,9 +46,9 @@ const fillCache = async (client: Client, amount?: number) => {
       continue;
     }
 
-    const timeout: number = reminder.notifyOn.getTime() - new Date().getTime();
-    setTimeout(() => handler(client, reminder), timeout);
-    remindersSchedulerCache.set(reminder.id, reminder, timeout);
+    const ttl: number = reminder.notifyOn.getTime() - new Date().getTime();
+    const timeout: NodeJS.Timeout = setTimeout(() => handler(client, reminder), ttl);
+    remindersSchedulerCache.set(reminder.id, { reminder, timeout }, ttl);
   }
 
 };
